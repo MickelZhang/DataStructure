86c1cf9ae87404748dbc11e95fddc9c29dcbbac0
MickelZhang <1223256867@qq.ccom>
2021-11-17 20:29:03 +0800

    将算法的相关代码迁移过来

---
+++ C++/Test/demo/about_exception.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------
+// Copyright (C) MickelZhang
+// License:MIT
+// 文件名：about_exception.h
+// 文件功能描述：异常处理
+// 创建者：MickelZhang
+// 时间：2020/12/22
+// 修改人：
+// 时间：
+// 修改说明：
+// 软件版本：VS2015
+//----------------------------------------------------------------*/
+#ifndef ABOUT_EXCEPTION_H
+#define  ABOUT_EXCEPTION_H
+
+//除数为0测试
+void TestForDivision();
+//异常处理
+void TestForExceptionOfDivision();
+//属于自己的异常类
+void TestForMyException();
+
+#endif
+
+
+
